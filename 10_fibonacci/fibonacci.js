922fde3493e8c45f2dc40b5da7e24f03ffb0ec03
trynil <ilovepudden@gmail.com>
2023-05-24 07:53:09 +0900

    up date test code answers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let n1 = 0, n2 = 1, nextTerm, number=Number(x);
+    if (Math.sign(x)=== -1) {
+        return "OOPS";
+    if (number === 1){
+        return 1}
+    
+     }
+    for (let i = 1; i < number; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return nextTerm
 
 };
 
